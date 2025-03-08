7cbb15afb23e2b22bcf7365aa4e6a8797a14ef52
Collin Hargreaves <chargrea@usc.edu>
2025-03-07 23:47:15 -0800

    heap

--- heap.h
+++ heap.h
@@ -61,13 +61,118 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
+  ///
+  std::vector<T> items_;
+  int m_;
+  PComparator comp_;
 
+  size_t parent(size_t idx) const {
+    return (idx - 1) / m_;
+  }
+
+  size_t kthChild(size_t idx, size_t k) const {
+    return m_ * idx + k;
+  }
+  
+  void heapifyUp(size_t idx);
+  void heapifyDown(size_t idx);
+  
 
 
 
 };
 
 // Add implementation of member functions here
+
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m, PComparator c) : m_(m), comp_(c) {
+
+  if (m < 2) {
+    throw std::invalid_argument("m must be >= 2");
+  }
+
+}
+
+template <typename T, typename PComparator>
+Heap<T,PComparator>::~Heap() {
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::push(const T& item) {
+  items_.push_back(item);
+  heapifyUp(items_.size() - 1);
+
+}
+
+template <typename T, typename PComparator>
+size_t Heap<T,PComparator>::size() const {
+  return items_.size();
+
+}
+
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::heapifyUp(size_t idx) {
+
+  // called after pushing a new element to maintain heap property
+  // compares new element with parents and swaps if we need
+  // continue until
+  // 1. reach the root idx = 0
+  // 2. the element is in correct position
+  
+  // loop until root
+  while(idx > 0) {
+    int p = parent(idx);
+    
+    // if the curr element is better than parent, we swap
+    if(comp_(items_[idx], items_[p])) {
+
+      std::swap(items_[idx], items_[p]);
+      idx = p;
+
+    }
+
+    else {
+      break;
+    }
+  }
+}
+
+template <typename T, typename PComparator>
+void Heap<T,PComparator>::heapifyDown(size_t idx) {
+
+  // done after removing the root element
+  // starts at root and moves an element down until heap restored
+  // we need to compare all children up to m children
+
+  size_t smallestChild;
+
+  // loop while node has at least one child
+  while(kthChild(idx, 1) < items_.size()) {
+    smallestChild = kthChild(idx, 1);  
+    
+    for(int k = 2; k <= m_ && kthChild(idx, k) < items_.size(); k++) {
+      int currentChild = kthChild(idx, k);
+
+      if(comp_(items_[currentChild], items_[smallestChild])) {
+        smallestChild = currentChild;
+      }
+
+    }
+    
+    if(comp_(items_[smallestChild], items_[idx])) {
+
+      std::swap(items_[idx], items_[smallestChild]);
+      idx = smallestChild;
+
+    }
+
+
+    else {
+      break;
+    }
+  }
+}
 
 
 // We will start top() for you to handle the case of 
@@ -81,12 +186,15 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
+    throw std::underflow_error("Heap is empty");
+
 
 
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
 
+  return items_[0];
 
 
 }
@@ -101,8 +209,15 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
+    throw std::underflow_error("Heap is empty");
 
+  }
 
+  items_[0] = items_.back();
+  items_.pop_back();
+
+  if (!empty()) {
+    heapifyDown(0);
   }
 
 
